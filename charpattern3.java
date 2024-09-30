6b1b60617b3a8e8bc5f58ee960aec970b71b4277
Dhairyasheel Jadhav <jadhavdhairyasheel41@gmail.com>
2024-09-30 02:34:33 -0400

    Add files via upload

---
+++ charpattern3.java
@@ -0,0 +1,14 @@
+public class charpattern3 {
+    public static void main(String[] args) {
+       
+            int n = 5;
+            for (int i =1 ,p='A';i<=n;i++,p+=2){
+    
+                for(int j =1;j<=i;j++){
+                    System.out.print((char)p+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
---
+++ numberpattern3.java
@@ -0,0 +1,12 @@
+public class numberpattern3 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i =1 ,p=2;i<=n;i++,p+=2){
+
+            for(int j =1;j<=i;j++){
+                System.out.print(p+" ");
+            }
+            System.out.println();
+        }
+    }
+}
